516f64eca48e04aa631843198fd2842c6953a2ac
Makai P. <makaipindell@gmail.com>
2026-06-12 14:42:05 -0400

    delete everything and restart

--- main.py
+++ main.py
@@ -18,47 +18,6 @@ def main():
     with open("config.yaml", "r") as f:
         config = yaml.safe_load(f)
     
-    camera = Camera(
-        config["camera"]["samples"],
-        config["camera"]["image_width"],
-        config["camera"]["image_height"],
-        config["camera"]["vfov"],
-        config["camera"]["max_depth"],
-        config["camera"]["gamma"]
-    )
-
-    world = HittableList(max_objects=100)
-    
-    world.add(Sphere(center=ti.Vector([0.0, 0.0, -1.0]), radius=0.5))
-    world.add(Sphere(center=ti.Vector([1.0, 1.0, -1.0]), radius=0.25))
-    world.add(Sphere(center=ti.Vector([0.0, -5.5, -1.0]), radius=5.0))
-    
-    live = True
-
-    if live:
-        gui = ti.GUI(
-            "raytrace test", 
-            (camera.image_width, camera.image_height),
-            fast_gui=True
-        )
-
-        while True:
-            if gui.get_event(ti.GUI.ESCAPE):
-                break
-
-            camera.handle_motion(gui)
-            camera.render(world, camera.kappa)
-            gui.set_image(camera.pixels)
-            gui.show()
-
-            print(f"  {camera.kappa} | <{camera.position}> | <{camera.rotation}>             ", end="\r")
-    else:
-        camera.render(world, camera.kappa)
-        img = camera.pixels.to_numpy()
-        img = img.transpose(1, 0, 2)
-        img = np.flip(img, axis=0)
-        img = (img * 255).astype(np.uint8)
-        Image.fromarray(img).save("out.png")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -4,45 +4,6 @@ import taichi.math as tm
 
 vec3 = ti.types.vector(3, float)
 
-@ti.func
-def sample_square():
-    return vec3(
-        ti.random(dtype=float) - 0.5, 
-        ti.random(dtype=float) - 0.5, 
-        0.0
-    )
-
-
-@ti.func
-def random_vector(
-    r_min: float = 0.0,
-    r_max: float = 1.0
-):
-    return vec3(
-        (ti.random(dtype=float) * (r_max - r_min)) + r_min,
-        (ti.random(dtype=float) * (r_max - r_min)) + r_min,
-        (ti.random(dtype=float) * (r_max - r_min)) + r_min
-    )
-
-
-@ti.func
-def random_on_hemi(normal: vec3):
-    on_unit_sphere = random_vector(-1.0, 1.0).normalized()
-    result = -on_unit_sphere
-
-    if tm.dot(on_unit_sphere, normal) > 0.0:
-        result = on_unit_sphere
-    
-    return result
-
-@ti.func
-def linear_to_gamma(lin_comp):
-    result = 0
-    
-    if lin_comp > 0:
-        result =  tm.sqrt(lin_comp)
-
-    return result
 
 
 @ti.func
